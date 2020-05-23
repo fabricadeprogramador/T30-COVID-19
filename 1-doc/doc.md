7eb47dcaf96eeaedc24b2c0c3443d033dbbf8402
High Tech Cursos <dev@hightechcursos.com.br>
2020-05-23 16:50:03 -0400

    Implementacao dos requisitos RF01,RF02,RF06,RF07

--- 1-doc/doc.md
+++ 1-doc/doc.md
@@ -3,15 +3,16 @@
 ## RF - Requisitos funcionais
 
     - RF01 - O usuário cadastra o Paciente (nome,sexo)
-    - RF02 - O usuário informa o número de vagas totais
+    - RF02 - O usuário informa o número de vagas no hospital normal
     - RF03 - O usuário remove o Paciente quando tiver alta
-    - RF04 - O Sistema liber mais uma vaga quando o usuaário é removido
+    - RF04 - O Sistema libera mais uma vaga quando o usuário é removido
     - RF05 - O Sistema calcula a porcentagem da ocupação
-    - RF06 - O sistema passar a cadastrar no Hospital de campanha após 70% de lotação
+    - RF06 - O sistema passa a cadastrar no Hospital de campanha após 70% de lotação
     - RF07 - O Usuário informa o numero de vagas do hospital de campanha
     - RF08 - O Sistema mostra a quantidade de pessoas no hospital de campanha
     - RF09 - O Usuário pode excluir do hospital da campanha.
-    - RF10 - O exibe as listas de pessoas
+    - RF10 - O Sistema exibe as listas de paciente no hospital normal
+    - RF11 - O Sistema exibe as listas de paciente no hospital de campanha
 
 ## RNF - Não Funcionais
 
--- 4-impl/hospital.js
+++ 4-impl/hospital.js
@@ -15,16 +15,46 @@ class Paciente {
 }
 
 
+
+var h1;
+var h2;
+
+//RF02 , RF07
 function cadastrarHospital() {
     //acessar as vagas da tela
     let vagasNormal = parseInt(document.getElementById("vagasNormal").value)
     let vagasCampanha = parseInt(document.getElementById("vagasCamp").value)
 
     //instancia 2 hospitais e preenche as vagas
-    let h1 = new Hospital(vagasNormal, 0, "N", [])
-    let h2 = new Hospital(vagasCampanha, 0, "C", [])
+     h1 = new Hospital(vagasNormal, 0, "N", [])
+     h2 = new Hospital(vagasCampanha, 0, "C", [])
 
     console.log(h1)
     console.log(h2)
 
 }
+
+//RF01
+function adicionarPaciente(){
+    //Acessar os dados do form
+    let nome =  document.getElementById("nome").value
+    //Acessa o sexo radio checked
+    let sexo = document.querySelector("input[type=radio]:checked").value
+    //Instancia um objeto contendo os dados do form
+    let p =  new Paciente (nome, sexo)
+    //imprimindo no console 
+   // console.log(p)
+    //Adicionando paciente no hospital
+    if (h1.pacientes.length < h1.vagas * 0.7) {
+        h1.pacientes.push(p)
+        console.log("Cadastrado no H1")
+    }else {
+        h2.pacientes.push(p)
+        console.log("Cadastrado no H2")
+    }
+}
+
+//RF10 - Implementar dia 30/05
+
+
+
